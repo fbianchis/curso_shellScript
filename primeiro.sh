ec7e652022b034b62c6337ec4cb68098778d2f5f
Felipe Bianchi <fbs96@live.com>
2020-02-25 20:52:28 -0300

    Adicionado comentários ao código

--- primeiro.sh
+++ primeiro.sh
@@ -1,8 +1,16 @@
 #!/bin/zsh
+#
+#Esse primeiro programa serve para mostrar conceitos básicos 
+#
+#Uso: ./primeiro.sh
+#
+#Usamos a primeira variável para atribuir um nome
 NOME="bia"
 echo "Olá ${NOME}"
+#Alteração no valor da variável
 NOME="vivek"
 echo "Olá ${NOME}"
+#Coleta de um valor de entrada do usuário
 echo "Digite seu nome"
 read MEU_NOME
 echo "Olá ${MEU_NOME}"
