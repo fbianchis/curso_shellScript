f79cfa6f45f0d94177c1af9ea84127d5e04a6b92
Felipe Bianchi <fbs96@live.com>
2020-02-25 22:42:56 -0300

    Argumentos posicionais

---
+++ argumentos.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+#
+#Um simples programa de argumentos
+echo "Argumento $1"
+echo "Argumento $2"
+#
+#número de argumentos passados
+echo "Foram passados um total de $# argumentos"
+#
+#Lista de todos os argumentos
+echo "A lista de argumentos foram: $@"
