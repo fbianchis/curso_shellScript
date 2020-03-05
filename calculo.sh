f4c2458b1029ad7c2b443b9679039cb8fa83e6a1
Felipe Bianchi <fbs96@live.com>
2020-03-05 10:39:57 -0300

    Expressões Aritméticas

---
+++ calculo.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+#Simples programa com calculos
+
+PRIMEIRO=1
+SEGUNDO=2
+
+echo "ADICIONAR"
+echo $((PRIMEIRO+SEGUNDO))
+
+echo "SUBTRAIR"
+echo $((PRIMEIRO-SEGUNDO))
+
+echo "MUlTIPLICAR"
+echo $((PRIMEIRO*SEGUNDO))
+
+echo "DIVIDIR"
+echo $((PRIMEIRO/SEGUNDO))
